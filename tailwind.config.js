627c6fe78fe067c419762800cf59091622ec0fad
Ruth <ruthamenu49@gmail.com>
2024-10-27 12:42:35 +0300

    second  commit

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    './src/**/*.{html,js,jsx,ts,tsx}', // Adjust paths based on your project structure
+    './public/index.html' // Add any other paths where you’re using Tailwind
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
